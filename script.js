2f33b4ec98cf86e746c96807da5b58f720f87756
lmiskelis <lukas.miskelis@gmail.com>
2022-05-27 13:44:36 +0300

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  marke: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+console.log(audi)
+function showObjectKeys(arr){
+let masyvas=Object.keys(arr)
+return masyvas
+}
+console.log(showObjectKeys(audi))
